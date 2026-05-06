58a09131f769d3cf9726e346a7e8c4b929a05578
WilliamGao1130 <gaoyiwei1130@outlook.com>
2026-05-06 16:10:41 +0800

    增加dashboard可调目标飞轮速度/add editable speed in Shootertester when using dashboard

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/Turret/Shootertester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/Turret/Shootertester.java
@@ -1,13 +1,17 @@
 package org.firstinspires.ftc.teamcode.controllers.Turret;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
+@Config
 @TeleOp(name = "Shootertester", group = "Tests")
 public class Shootertester extends LinearOpMode {
-
+    public static double high_speed = 1800;
+    public static double medium_high_speed = 1200;
+    public static double medium_low_speed = 800;
+    public static double low_speed = 400;
     public FlyWheelModule flyWheelModule;
 
     public void runOpMode() throws InterruptedException {
@@ -18,15 +22,18 @@ public class Shootertester extends LinearOpMode {
         while (opModeIsActive()) {
             // 测试不同速度
             if (gamepad1.aWasPressed()) {
-                flyWheelModule.setTargetSpeed(1500); // 高速
+                flyWheelModule.setTargetSpeed((int) high_speed); // 高速
             } else if (gamepad1.bWasPressed()) {
-                flyWheelModule.setTargetSpeed(1000); // 中高速
+                flyWheelModule.setTargetSpeed((int) medium_high_speed); // 中高速
             } else if (gamepad1.yWasPressed()) {
-                flyWheelModule.setTargetSpeed(650); // 中速
+                flyWheelModule.setTargetSpeed((int) medium_low_speed); // 中速
             } else if (gamepad1.xWasPressed()) {
-                flyWheelModule.setTargetSpeed(400); // 低速
+                flyWheelModule.setTargetSpeed((int) low_speed); // 低速
             } else if (gamepad1.dpad_up) {
                 flyWheelModule.setTargetSpeed(0); // 停止
+            }
+            if(gamepad1.left_stick_y!=0){
+                flyWheelModule.setTargetSpeed((int)(-gamepad1.left_stick_y*high_speed));
             }
             flyWheelModule.update();
 
